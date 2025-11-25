2da1f466091f22fa62750c80331c6fce9e84cec1
BobbyCoppersmithEnv0 <robert.coppersmith@env0.com>
2025-11-25 10:35:49 -0800

    Update variables.tf

--- ec2-with-ebs/variables.tf
+++ ec2-with-ebs/variables.tf
@@ -12,11 +12,6 @@ variable "vpc_id" {
   type = string
 }
 
-variable "availability_zone" {
-  description = "The specific Availability Zone (e.g., us-west-2a) to launch the EC2 instance in."
-  type        = string
-}
-
 # NEW > Artem [Below] : Data volume size (in GiB) for the extra EBS volume
 variable "data_volume_size" {
   description = "Size of the additional EBS data volume in GiB."
